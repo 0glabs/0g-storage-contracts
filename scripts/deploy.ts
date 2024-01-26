7941251d41622a72278e2dee902a8407ea79c52d
MiniFrenchBread <103425574+MiniFrenchBread@users.noreply.github.com>
2024-01-26 18:09:43 +0800

    chore: fix deploy script

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -6,14 +6,14 @@ async function main() {
   let token = await erc20ABI.deploy();
 
   let flowABI = await ethers.getContractFactory("Flow");
-  const blocksPerEpoch = 1_000_000;
-  let flow = await flowABI.deploy(token.address, blocksPerEpoch, 0);
+  const blocksPerEpoch = 100;
+  let flow = await flowABI.deploy("0x0000000000000000000000000000000000000000", blocksPerEpoch, 0);
 
   await token.approve(flow.address, 1e9);
 
   let mineABI = await ethers.getContractFactory("PoraMineTest");
   // TODO: deploy new contracts
-  let mine = await mineABI.deploy(flow.address, "0x0000000000000000000000000000000000000000", 7);
+  let mine = await mineABI.deploy(flow.address, "0x0000000000000000000000000000000000000000", 4);
 
   const output = `token = '${token.address}'\nflow = '${flow.address}'\nPoraMine = '${mine.address}'`;
 
