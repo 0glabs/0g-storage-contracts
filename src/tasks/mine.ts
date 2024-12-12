2de3830fc799ab982b526ae771d91b2a82f1eb14
Peter Zhang <peter@0g.ai>
2024-12-12 11:25:58 +0800

    add mine script

--- src/tasks/mine.ts
+++ src/tasks/mine.ts
@@ -4,17 +4,13 @@ import { CONTRACTS, getTypedContract } from "../utils/utils";
 task("mine:show", "show contract params").setAction(async (_, hre) => {
     const mine = await getTypedContract(hre, CONTRACTS.PoraMine);
     console.log(await mine.targetSubmissions());
+    console.log(await mine.canSubmit.staticCall());
 });
 
 task("mine:setTargetSubmissions", "set target submissions")
     .addParam("n", "number of target submissions", undefined, types.int, false)
     .setAction(async (taskArgs: { n: number }, hre) => {
         const mine = await getTypedContract(hre, CONTRACTS.PoraMine);
-        let canSubmit = await mine.canSubmit();
-        if (!canSubmit) {
-            await (await mine.setTargetSubmissions(taskArgs.n)).wait();
-            console.log(`set target submission to ${taskArgs.n}`);
-        } else {
-            console.log("cannot set target submission");
-        }
+        await (await mine.setTargetSubmissions(taskArgs.n)).wait();
+        console.log(`set target submission to ${taskArgs.n}`);
     });
