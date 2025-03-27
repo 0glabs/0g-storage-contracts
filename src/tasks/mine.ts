733a5431d7451222a7a57222dc0db7ea6f34db82
0g-peterzhb <158457852+0g-peterzhb@users.noreply.github.com>
2025-03-27 21:50:32 +0800

    add mine and reward tasks (#51)

--- src/tasks/mine.ts
+++ src/tasks/mine.ts
@@ -14,3 +14,19 @@ task("mine:setTargetSubmissions", "set target submissions")
         await (await mine.setTargetSubmissions(taskArgs.n)).wait();
         console.log(`set target submission to ${taskArgs.n}`);
     });
+
+task("mine:setMinDifficulty", "set min difficulty")
+    .addParam("min", "number of min difficulty", undefined, types.bigint, false)
+    .setAction(async (taskArgs: { min: bigint }, hre) => {
+        const mine = await getTypedContract(hre, CONTRACTS.PoraMine);
+        await (await mine.setMinDifficulty(taskArgs.min)).wait();
+        console.log(`set min difficulty to ${taskArgs.min}`);
+    });
+
+task("mine:setNumSubtasks", "set num subtasks")
+    .addParam("n", "number of num subtasks", undefined, types.int, false)
+    .setAction(async (taskArgs: { n: number }, hre) => {
+        const mine = await getTypedContract(hre, CONTRACTS.PoraMine);
+        await (await mine.setNumSubtasks(taskArgs.n)).wait();
+        console.log(`set num subtasks to ${taskArgs.n}`);
+    });
